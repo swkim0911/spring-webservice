e3ce1da6ac1ba21461b28b7d1d7b5180a45b3c35
swkim0911 <kimsungwon364@gmail.com>
2023-03-04 18:25:32 +0900

    Initial commit

---
+++ src/test/java/org/example/springboot/Dto/HelloResponseDtoTest.java
@@ -0,0 +1,26 @@
+package org.example.springboot.Dto;
+
+import org.assertj.core.api.Assertions;
+import org.example.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+
+}
